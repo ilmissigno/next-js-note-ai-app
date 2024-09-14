eafe9678de0ff4e971a43cc9e6f7fbb49ad3af1b
ilmissigno <raffaele.formisano.1994@gmail.com>
2024-09-14 17:15:23 +0200

    23

--- src/lib/db/pinecone.ts
+++ src/lib/db/pinecone.ts
@@ -2,9 +2,9 @@ import {Pinecone} from "@pinecone-database/pinecone"
 
 const apiKey = process.env.PINECONE_API_KEY
 
-// if (!apiKey){
-//     throw Error("PINECONE API KEY is not set")
-// }
+if (!apiKey){
+    throw Error("PINECONE API KEY is not set")
+}
 
 const pinecone = new Pinecone({
     apiKey
